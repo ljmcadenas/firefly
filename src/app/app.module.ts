f7a7f0cb55227c74800617e4e2a63b8e07847775
Leo Cadenas <ljmcadenas@gmail.com>
2022-07-11 12:58:21 +1200

    Removed unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OverviewComponent } from './components/overview/overview.component';
-import { TopMusicComponent } from './components/top-music/top-music.component';
 import { MessageBannerModule } from './core/message-banner/message-banner.module';
 import { SideNavigationModule } from './core/side-navigation/side-navigation.module';
 
 @NgModule({
-	declarations: [AppComponent, OverviewComponent, TopMusicComponent],
+	declarations: [AppComponent],
 	imports: [
 		CommonModule,
 		AppRoutingModule,
